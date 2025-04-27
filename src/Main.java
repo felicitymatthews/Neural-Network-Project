a1af6a65c62cb4b43244ffdc20d3c1abd3e2616a
808Ekimura <kimurae@berkeley.edu>
2025-04-27 13:30:24 -0700

    to-do
    made a txt file but wouldnt show changes in desktop so had to add to main as comments

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+/*
+- neuron and synapse class
+
+- can use either layer class for each synpase/neuron layer (will have to have subclasses for input and 
+output layer) or an overall neural network class. Could also connect neurons and synapses individually with
+each neuron/synpase containing adjacent neuron/synpase as a attr.
+
+- implement forward and backward propagation (research math calculations and activation functions)
+
+- decide on data set to train/test and build relevent layers
+
+- test/train
+
+-? implement graphing library to debug training/testing
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
